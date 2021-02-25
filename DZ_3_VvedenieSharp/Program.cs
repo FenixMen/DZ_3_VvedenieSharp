fb6067447e277d637e9b9148ff9a15c2e4feb559
Fenix <arhange1l@yandex.ru>
2021-02-25 22:36:04 +0300

    Задание 1

--- DZ_3_VvedenieSharp/Program.cs
+++ DZ_3_VvedenieSharp/Program.cs
@@ -3,9 +3,30 @@
 namespace DZ_3_VvedenieSharp
 {
     class Program
-    {
+    {//Написать программу, выводящую элементы двухмерного массива по диагонали.
+
         static void Main(string[] args)
         {
+            string[,] myArray = new string[5 , 5];
+            Random rndValue = new Random();
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    myArray[i, j] = Convert.ToString(rndValue.Next(0, 600));
+                }
+                
+            }
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    Console.WriteLine($"Индекс {i},{j} = {myArray[i, j]}");
+                }
+
+            }
 
         }
     }
