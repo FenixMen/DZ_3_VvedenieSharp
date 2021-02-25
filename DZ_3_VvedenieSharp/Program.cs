11fd0a74c6e2941877ea976ac3b1f949f67faadd
Fenix <arhange1l@yandex.ru>
2021-02-25 23:02:40 +0300

    Задание 2

--- DZ_3_VvedenieSharp/Program.cs
+++ DZ_3_VvedenieSharp/Program.cs
@@ -3,10 +3,17 @@
 namespace DZ_3_VvedenieSharp
 {
     class Program
-    {
+    {//Написать программу — телефонный справочник — создать двумерный массив 5*2, хранящий список телефонных контактов: 
+     //первый элемент хранит имя контакта, второй — номер телефона/e-mail.
+
         static void Main(string[] args)
         {
-
+            string[,] phoneBook = new string[5, 2] { 
+                {"Alex", "5483621/mail" },
+                {"Rudi", "873484/mailnext"}, 
+                {"Коляныч" , "секретно"},
+                {"Gloria" , "7441216"},
+                {"Atskiy_Satana", "666"} };
         }
     }
 }
