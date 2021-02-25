ef2088f8bb07ad6f5a222fe24695cf4cffcdabd6
Fenix <arhange1l@yandex.ru>
2021-02-25 23:11:13 +0300

    Задание 3

--- DZ_3_VvedenieSharp/Program.cs
+++ DZ_3_VvedenieSharp/Program.cs
@@ -3,10 +3,17 @@
 namespace DZ_3_VvedenieSharp
 {
     class Program
-    {
+    {//Написать программу, выводящую введенную пользователем строку в обратном порядке (olleH вместо Hello).
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите любое слово/симвлы:");
+            string userAnswer = Console.ReadLine();
 
+            for (int i = userAnswer.Length -1; i >= 0; i--)
+            {
+                Console.Write(userAnswer[i]);
+            }
         }
     }
 }
